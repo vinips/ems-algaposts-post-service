cc30a26d2c641392ba2c03d1e04c9b048cfafe69
Vini Silveira <viniciusps_ph@hotmail.com>
2025-07-24 19:41:31 -0300

    Configuração inicial do projeto.

---
+++ src/test/java/com/algaworks/algaposts/post/service/PostServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.algaworks.algaposts.post.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//Primeiro Commit
+	}
+
+}
